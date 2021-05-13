f397d27afd5576d57938ca673c0f0e4e8ae70eaf
perlancar (on pc-home) <perlancar@gmail.com>
2021-05-13 08:47:05 +0700

    Release v0.001

--- lib/Module/Features/Set.pm
+++ lib/Module/Features/Set.pm
@@ -12,13 +12,30 @@ use warnings;
 our %FEATURES_DEF = (
     v => 1,
     summary => 'Features of modules that handle set data structure',
-    features => {
-        can_insert_value                         => {summary => 'Provide a way for user to insert a value to a set', tags=>['category:basic']},
-        can_delete_value                         => {summary => 'Provide a way for user to delete a value from a set', tags=>['category:basic']},
-        can_search_value                         => {summary => 'Provide a way for user to search a value in a set', tags=>['category:basic']},
-        can_count_values                         => {summary => 'Provide a way for user to get the number of values in a set', tags=>['category:basic']},
+    description => <<'_',
 
-        can_compare_sets                         => {summary => 'Provide a way for user to check the equality of two sets', tags=>['category:sets']},
+To check whether two sets are equal, you can use intersection
+(`can_intersect_sets`). Two sets are equal if their intersection has the same
+size as either set.
+
+To check whether a set is a subset of another, you can use difference
+(`can_difference_sets`). A set is a subset of another if the other set's
+difference with the set has the same size as the first set.
+
+To check whether set A is a superset of set B, check whether set B is a subset
+of set A.
+
+_
+    features => {
+        can_insert_value                         => {summary => 'Provide a way for user to insert a value to a set', tags=>['category:intraset-operation']},
+        can_delete_value                         => {summary => 'Provide a way for user to delete a value from a set', tags=>['category:intraset-operation']},
+        can_search_value                         => {summary => 'Provide a way for user to search a value in a set', tags=>['category:intraset-operation']},
+        can_count_values                         => {summary => 'Provide a way for user to get the number of values in a set (the size of the set)', tags=>['category:intraset-operation']},
+
+        can_union_sets                           => {summary => 'Provide a way for user to perform union operation of two or more sets', tags=>['category:interset-operation']},
+        can_intersect_sets                       => {summary => 'Provide a way for user to perform intersection operation of two or more sets', tags=>['category:interset-operation']},
+        can_difference_sets                      => {summary => 'Provide a way for user to perform difference operation of two or more sets (values in first set not in the rest)', tags=>['category:interset-operation']},
+        can_symmetric_difference_sets            => {summary => 'Provide a way for user to perform symmetric difference operation of two or more sets (values that are only in exactly one set)', tags=>['category:interset-operation']},
 
         speed                                    => {summary => 'Subjective speed rating, relative to other set modules', schema=>['str', in=>[qw/slow medium fast/]], tags=>['category:speed']},
 
